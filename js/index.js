82d0ca986b8a8f02ab3d7bed8bac5da05724ec50
Anik Hassan1 <ah491606@gmail.com>
2024-03-20 21:44:28 +0600

    Home page

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// function click(){
+//    var element= document.getElementById('col-btns');
+//    console.log(element);
+//    element.classList("active");
+// }
+// click();
+// let a=document.getElementById('a');
+// let ov=document.getElementById('o');
+// console.log(a);
+// a.addEventListener("click",function(){
+//     console.log("anik");
+//     ov.classList.add("active");
+// })
